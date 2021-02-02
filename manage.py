05cc5357f316d72efa811a354df0a8c6ea39d8fb
Sebastianm <juansejunior10@gmail.com>
2021-02-02 00:45:05 -0500

    Configuracion api rest

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nous_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nous_backend.settings.local.py')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
